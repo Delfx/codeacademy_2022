25fa704b86e6e78decdfb07ff08ca98ff1a563aa
Delfx <motiejunas.lukas@gmail.com>
2022-05-25 19:10:40 +0300

    added minmax task

--- task_js/assets/js/app.js
+++ task_js/assets/js/app.js
@@ -5,11 +5,10 @@ function sum(){
 
     const numberone = document.getElementById('numberone');
     const numbertwo = document.getElementById('numbertwo');
-    const sump = document.getElementById('sum');
 
-    const sum = parseInt(numberone.value) + parseInt(numbertwo.value)
-    
-    sump.innerHTML = sum;
+    const sum = document.getElementById('sum');
+
+    sum.innerHTML = parseInt(numberone.value) + parseInt(numbertwo.value);
 
 
 }
---
+++ task_js/assets/js/minmax.js
@@ -0,0 +1,21 @@
+function getRandomFloat(min, max, decimals) {
+    const str = (Math.random() * (max - min) + min).toFixed(decimals);
+
+    return parseFloat(str);
+}
+
+
+let a = getRandomFloat(1, 10, 2)
+let b = getRandomFloat(1, 10, 2)
+let c = getRandomFloat(1, 10, 2)
+let d = getRandomFloat(1, 10, 2)
+let e = getRandomFloat(1, 10, 2)
+
+let numbers = [a, b, c, d, e];
+
+
+console.log(numbers)
+
+console.log(Math.min.apply(Math, numbers));
+console.log(Math.max.apply(Math, numbers));
+
