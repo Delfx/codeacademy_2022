63c8f68eb0508daa952e6ce670f88fa2611766f4
Delfx <motiejunas.lukas@gmail.com>
2022-05-30 19:04:19 +0300

    updated js menu function

--- homework/h1/js/menu.js
+++ homework/h1/js/menu.js
@@ -1,8 +1,19 @@
-function myFunction() {
-    let x = document.getElementById("nav-text-id");
-    if (x.style.display === "flex") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "flex";
-    }
+window.onresize = function () {
+  let navtext = document.getElementById("nav-text-id")
+  let w = window.innerWidth;
+  if (window.innerWidth > 768) {
+    navtext.style.display = "flex"
+  } else {
+    navtext.style.display = "none"
   }
+}
+
+function myFunction() {
+  let x = document.getElementById("nav-text-id");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
+
