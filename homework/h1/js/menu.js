cdbb94008029dbf51fe0a3ea989d71a09f96a4da
Delfx <motiejunas.lukas@gmail.com>
2022-05-30 18:09:11 +0300

    updated task homework

--- homework/h1/js/menu.js
+++ homework/h1/js/menu.js
@@ -1,6 +1,5 @@
 function myFunction() {
     let x = document.getElementById("nav-text-id");
-    console.log(x);
     if (x.style.display === "flex") {
       x.style.display = "none";
     } else {
