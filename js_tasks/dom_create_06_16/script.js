9a52112b68e99fcb794479a0c0354228bd6b332e
Delfx <motiejunas.lukas@gmail.com>
2022-06-16 21:33:44 +0300

    updated task

--- js_tasks/dom_create_06_16/script.js
+++ js_tasks/dom_create_06_16/script.js
@@ -13,12 +13,9 @@ function paragraphElements (text){
     return paragraphElement.appendChild(text)
 }
 
-
 h2Element.appendChild(textNodeH2);
 liElement.appendChild(textNodeLi);
-
 ulElement.appendChild(liElement);
-
 
 window.addEventListener("DOMContentLoaded", e => {
     mainElement.appendChild(h2Element)
