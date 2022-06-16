b53ded5fd73bd8fc33eec8e1ecc10b6194ebaca7
Delfx <motiejunas.lukas@gmail.com>
2022-06-16 21:30:30 +0300

    create with js Dom element

---
+++ js_tasks/dom_create_06_16/script.js
@@ -0,0 +1,32 @@
+const mainElement = document.getElementById("main");
+const h2Element = document.createElement("h2");
+const ulElement = document.createElement("ul");
+const liElement= document.createElement("li")
+const paragraphElement = document.createElement("p");
+
+const textNodeH2 = document.createTextNode("Hello World");
+const textNodeLi = document.createTextNode("I am text in Li element");
+const textParagElement = document.createTextNode("I am first paragraph Element")
+const textParagElementSecond = document.createTextNode("I am second paragraph Element")
+
+function paragraphElements (text){
+    return paragraphElement.appendChild(text)
+}
+
+
+h2Element.appendChild(textNodeH2);
+liElement.appendChild(textNodeLi);
+
+ulElement.appendChild(liElement);
+
+
+window.addEventListener("DOMContentLoaded", e => {
+    mainElement.appendChild(h2Element)
+    mainElement.appendChild(paragraphElements(textParagElement))
+    mainElement.appendChild(ulElement)
+    mainElement.appendChild(paragraphElements(textParagElementSecond))
+})
+
+
+
+
