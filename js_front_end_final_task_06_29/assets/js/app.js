3f644cfe82182dbf82528d7bb7466c88f8eecc69
Delfx <motiejunas.lukas@gmail.com>
2022-06-30 22:30:11 +0300

    updated task front end js final

--- js_front_end_final_task_06_29/assets/js/app.js
+++ js_front_end_final_task_06_29/assets/js/app.js
@@ -16,7 +16,7 @@ const getInputForConvert = document.querySelector('#convertTextInput');
 const getUpperCaseButton = document.querySelector('#upperCaseAll');
 
 function upperCaseAll() {
-   return getInputForConvert.value = getInputForConvert.value.toUpperCase();
+    return getInputForConvert.value = getInputForConvert.value.toUpperCase();
 }
 
 getUpperCaseButton.addEventListener('click', upperCaseAll);
@@ -38,7 +38,7 @@ const getupperCaseFirstButton = document.querySelector('#upperCaseFirst');
 function capitalizeFirstLetter() {
 
     // converting first letter to uppercase
-  return getInputForConvert.value = getInputForConvert.value.charAt(0).toUpperCase() + getInputForConvert.value.slice(1);
+    return getInputForConvert.value = getInputForConvert.value.charAt(0).toUpperCase() + getInputForConvert.value.slice(1);
 
 }
 
@@ -52,10 +52,54 @@ const getLowerCaseFirstButton = document.querySelector('#lowerCaseFirst');
 function lowerFirstLetter() {
 
     // converting first letter to lowercase
-  return getInputForConvert.value = getInputForConvert.value.charAt(0).toLowerCase() + getInputForConvert.value.slice(1);
+    return getInputForConvert.value = getInputForConvert.value.charAt(0).toLowerCase() + getInputForConvert.value.slice(1);
 
 }
 
 getLowerCaseFirstButton.addEventListener('click', lowerFirstLetter);
+
+//block or unblock with button and readonly property
+const readonlyInput = document.querySelector('#readonly-input')
+const blockButton = document.querySelector('#button-block')
+const unblockButton = document.querySelector('#button-unblock')
+
+blockButton.addEventListener('click', () => {
+    readonlyInput.readOnly = true;
+})
+
+unblockButton.addEventListener('click', () => {
+    readonlyInput.readOnly = false;
+})
+
+//picture onhover show hide 
+const firsPicture = document.querySelector('#first-picture');
+const secondPicture = document.querySelector('#second-picture');
+
+firsPicture.addEventListener('mouseover', (e) => {
+    secondPicture.style.display = 'inline'
+    firsPicture.style.display = 'none'
+})
+
+secondPicture.addEventListener('mouseleave', () => {
+    secondPicture.style.display = 'none'
+    firsPicture.style.display = 'inline'
+})
+
+//style button functions
+const textToStyle = document.querySelector('#lorum-text');
+const colorButton = document.querySelector('#color-dropdown');
+
+
+colorButton.addEventListener('click', (e) => {
+
+    if(e.target.value == 1){
+        textToStyle.setAttribute("style","color:red !important")
+    }else if(e.target.value == 2){
+        textToStyle.setAttribute("style","color:green !important")
+    }else if(e.target.value == 3){
+        textToStyle.setAttribute("style","color:blue !important")
+    }
+    
+})
 
 
