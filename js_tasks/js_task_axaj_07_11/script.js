8f7c834041a5239bbc333604d2c323a4b362a0a9
Lukas <motiejunas.lukas@gmail.com>
2022-07-12 09:19:27 +0300

    ajax task 07 11

---
+++ js_tasks/js_task_axaj_07_11/script.js
@@ -0,0 +1,41 @@
+
+async function getPost() {
+    try {
+        let response = await fetch('https://jsonplaceholder.typicode.com/posts')
+        if (!response.ok)
+            throw new Error("HTTP error " + response.status);
+        let body = await response.json(); // or .json() or whatever
+        return body
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+async function getAll() {
+    const posts = await getPost();
+    const postTable = document.querySelector('#table-posts');
+
+    for (const post of posts) {
+
+        const createTr = document.createElement('tr');
+        const createTdID = document.createElement('td')
+        const createTdUserId = document.createElement('td')
+        const createTdTitle = document.createElement('td')
+    
+        console.log(post.userId);
+        createTdID.textContent = post.id
+        createTdUserId.textContent = post.userId
+        createTdTitle.textContent = post.title
+
+        createTr.appendChild(createTdID)
+        createTr.appendChild(createTdUserId)
+        createTr.appendChild(createTdTitle)
+        postTable.appendChild(createTr)
+    }
+
+}
+
+getAll()
+
+
+
