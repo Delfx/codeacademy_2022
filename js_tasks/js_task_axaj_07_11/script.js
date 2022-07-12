eadbf2454329a22438827fea07db5e9805bc8ecd
Delfx <motiejunas.lukas@gmail.com>
2022-07-12 20:37:10 +0300

    updated task added name from jsonplaceholder

--- js_tasks/js_task_axaj_07_11/script.js
+++ js_tasks/js_task_axaj_07_11/script.js
@@ -2,38 +2,69 @@
 async function getPost() {
     try {
         let response = await fetch('https://jsonplaceholder.typicode.com/posts')
-        if (!response.ok){
+        if (!response.ok) {
             throw new Error("HTTP error " + response.status);
         }
         let body = await response.json(); // or .json() or whatever
         return body
     } catch (err) {
-        console.log(err)
+        return false;
     }
 }
+
+async function getUsers() {
+    try {
+        let response = await fetch('https://jsonplaceholder.typicode.com/users')
+        if (!response.ok) {
+            throw new Error("HTTP error " + response.status);
+        }
+        let users = await response.json(); // or .json() or whatever
+        return users;
+    } catch (err) {
+        return false;
+    }
+}
+
+
 
 async function showAllPosts() {
     const posts = await getPost();
+    const users = await getUsers();
     const postTable = document.querySelector('#table-posts');
 
-    for (const post of posts) {
-
+    if (!posts || !users) {
         const createTr = document.createElement('tr');
-        const createTdID = document.createElement('td')
-        const createTdUserId = document.createElement('td')
-        const createTdTitle = document.createElement('td')
-    
-        createTdID.textContent = post.id
-        createTdUserId.textContent = post.userId
-        createTdTitle.textContent = post.title
-
-        createTr.appendChild(createTdID)
-        createTr.appendChild(createTdUserId)
-        createTr.appendChild(createTdTitle)
+        createTr.textContent = 'error from server'
         postTable.appendChild(createTr)
-    }
 
+    } else {
+        for (const post of posts) {
+            const createTr = document.createElement('tr');
+            const createTdID = document.createElement('td')
+            const createTdUserName = document.createElement('td')
+            const createTdTitle = document.createElement('td')
+
+            for (const user of users) {
+                console.log(user.id);
+                console.log(post.userId);
+                if (post.userId == user.id) {
+                    createTdID.textContent = post.id
+                    createTdUserName.textContent = user.name
+                    createTdTitle.textContent = post.title
+                }
+            }
+
+            createTr.appendChild(createTdID)
+            createTr.appendChild(createTdUserName)
+            createTr.appendChild(createTdTitle)
+            postTable.appendChild(createTr)
+        }
+
+
+    }
 }
+
+
 
 showAllPosts()
 
